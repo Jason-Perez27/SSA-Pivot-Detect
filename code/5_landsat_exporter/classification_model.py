331a4902a6cc6c75f8590f78751ca2a9e035db48
Jason-Perez27 <137958043+Jason-Perez27@users.noreply.github.com>
2024-08-23 13:00:07 -0700

    updated data_request.py

---
+++ code/5_landsat_exporter/classification_model.py
@@ -0,0 +1,95 @@
+import ee
+import geopandas as gpd
+import os
+import random
+import re
+import time
+import logging
+
+# Trigger the authentication flow.
+ee.Authenticate()
+
+# Initialize the library.
+ee.Initialize(project='center-pivots')
+
+# Setup logging
+logging.basicConfig(filename='exporter.log', level=logging.INFO, format='%(asctime)s %(message)s')
+
+class LandsatDataExporter:
+    def __init__(self, shapefile_path, max_tasks=50):
+        self.shapefile_path = os.path.expanduser(shapefile_path)
+        self.center_pivot_gdf = gpd.read_file(self.shapefile_path)
+        self.max_tasks = max_tasks
+        self.landsat_collections = {
+            'LANDSAT/LT04/C01/T1_SR': (1982, 1993, 'Landsat 4'),
+            'LANDSAT/LT05/C01/T1_SR': (1984, 2012, 'Landsat 5'),
+            'LANDSAT/LC08/C01/T1_SR': (2013, 2021, 'Landsat 8'),
+            'LANDSAT/LE07/C01/T1_SR': (1999, 2022, 'Landsat 7'),
+            'LANDSAT/LC09/C01/T1_L2': (2021, 2023, 'Landsat 9')
+        }
+
+    def clean_description(self, desc):
+        cleaned_desc = re.sub(r'[^a-zA-Z0-9.,:;_-]', '_', desc)
+        return cleaned_desc[:100]
+
+    def divide_pivots(self):
+        num_pivots = len(self.center_pivot_gdf)
+        pivots_per_group = max(10, num_pivots // 100)  # Adjust the number of groups based on your needs
+        self.pivot_groups = [self.center_pivot_gdf.iloc[i:i+pivots_per_group] for i in range(0, num_pivots, pivots_per_group)]
+        if num_pivots % 100 != 0:
+            self.pivot_groups.append(self.center_pivot_gdf.iloc[-(num_pivots % 100):])
+
+    def export_landsat_data(self):
+        self.divide_pivots()
+        for collection_path, (start_year, end_year, landsat_name) in self.landsat_collections.items():
+            collection = ee.ImageCollection(collection_path)
+            for pivot_group in self.pivot_groups:
+                self.process_group(pivot_group, collection, start_year, end_year, landsat_name)
+
+    def process_group(self, pivot_group, collection, start_year, end_year, landsat_name):
+        for index, row in pivot_group.iterrows():
+            if self.check_active_tasks() < self.max_tasks:
+                try:
+                    self.export_pivot(row, collection, start_year, end_year, landsat_name)
+                except Exception as e:
+                    logging.error(f'Failed to process pivot {row["ID"]}: {str(e)}')
+                    continue
+            else:
+                logging.info("Max tasks reached, waiting...")
+                time.sleep(600)  # Wait for 10 minutes before checking again
+                self.process_group(pivot_group, collection, start_year, end_year, landsat_name)
+                break
+
+    def export_pivot(self, row, collection, start_year, end_year, landsat_name):
+        geom = row['geometry']
+        month = random.randint(1, 13)
+        year = random.randint(start_year, end_year)
+        buffer_distance = 1000 / 100000
+        ee_geom = ee.Geometry(geom.__geo_interface__)
+        filtered = collection.filterDate(f'{year}-{month:02d}-01', f'{year}-{month:02d}-28')\
+                             .filterBounds(ee_geom)\
+                             .filter(ee.Filter.lt('CLOUD_COVER', 10))
+        pivot_id = row['ID']
+        clipped_image = ee.Image(filtered.first()).toFloat()
+        description = f'CenterPivot_{pivot_id}_{year}_{month:02d}_{landsat_name}'
+        export_options = {
+            'image': clipped_image,
+            'description': self.clean_description(description),
+            'folder': 'TOTAL_TIF_REQUEST',
+            'scale': 30,
+            'region': ee_geom,
+            'maxPixels': 10000000000000
+        }
+        task = ee.batch.Export.image.toDrive(**export_options)
+        task.start()
+        logging.info(f'Started export task for pivot {pivot_id} for {landsat_name} in {year}-{month:02d}')
+
+    def check_active_tasks(self):
+        tasks = ee.batch.Task.list()
+        running_tasks = [task for task in tasks if task.state in ['RUNNING', 'READY']]
+        return len(running_tasks)
+
+# Usage
+shapefile_path = '/home/waves/data/SSA-Pivot-Detect/data/combined_CPIS/combined_CPIS.shp'
+exporter = LandsatDataExporter(shapefile_path)
+exporter.export_landsat_data()
--- code/5_landsat_exporter/data_request.py
+++ code/5_landsat_exporter/data_request.py
@@ -1,11 +1,8 @@
 import ee
 import geopandas as gpd
 import os
-import random
-import re
-import time
 import logging
-
+import time
 # Trigger the authentication flow.
 ee.Authenticate()
 
@@ -21,59 +18,42 @@ class LandsatDataExporter:
         self.center_pivot_gdf = gpd.read_file(self.shapefile_path)
         self.max_tasks = max_tasks
         self.landsat_collections = {
-            'LANDSAT/LT04/C01/T1_SR': (1982, 1993, 'Landsat 4'),
-            'LANDSAT/LT05/C01/T1_SR': (1984, 2012, 'Landsat 5'),
-            'LANDSAT/LC08/C01/T1_SR': (2013, 2021, 'Landsat 8'),
-            'LANDSAT/LE07/C01/T1_SR': (1999, 2022, 'Landsat 7'),
-            'LANDSAT/LC09/C01/T1_L2': (2021, 2023, 'Landsat 9')
+            'LANDSAT/LT04/C02/T1_L2': (1982, 1993, 'Landsat 4'),
+            'LANDSAT/LT05/C02/T1_L2': (1984, 2012, 'Landsat 5'),
+            'LANDSAT/LC08/C02/T1_L2': (2013, 2021, 'Landsat 8'),
+            'LANDSAT/LE07/C02/T1_L2': (1999, 2022, 'Landsat 7'),
+            'LANDSAT/LC09/C02/T1_L2': (2021, 2023, 'Landsat 9')
         }
 
     def clean_description(self, desc):
         cleaned_desc = re.sub(r'[^a-zA-Z0-9.,:;_-]', '_', desc)
         return cleaned_desc[:100]
 
-    def divide_pivots(self):
-        num_pivots = len(self.center_pivot_gdf)
-        pivots_per_group = max(10, num_pivots // 100)  # Adjust the number of groups based on your needs
-        self.pivot_groups = [self.center_pivot_gdf.iloc[i:i+pivots_per_group] for i in range(0, num_pivots, pivots_per_group)]
-        if num_pivots % 100 != 0:
-            self.pivot_groups.append(self.center_pivot_gdf.iloc[-(num_pivots % 100):])
-
     def export_landsat_data(self):
-        self.divide_pivots()
-        for collection_path, (start_year, end_year, landsat_name) in self.landsat_collections.items():
-            collection = ee.ImageCollection(collection_path)
-            for pivot_group in self.pivot_groups:
-                self.process_group(pivot_group, collection, start_year, end_year, landsat_name)
+        for index, row in self.center_pivot_gdf.iterrows():
+            for collection_path, (start_year, end_year, landsat_name) in self.landsat_collections.items():
+                if self.check_active_tasks() < self.max_tasks:
+                    try:
+                        self.export_pivot(row, collection_path, start_year, end_year, landsat_name)
+                    except Exception as e:
+                        logging.error(f'Failed to process pivot {row["ID"]}: {str(e)}')
+                else:
+                    logging.info("Max tasks reached, waiting...")
+                    time.sleep(600)  
 
-    def process_group(self, pivot_group, collection, start_year, end_year, landsat_name):
-        for index, row in pivot_group.iterrows():
-            if self.check_active_tasks() < self.max_tasks:
-                try:
-                    self.export_pivot(row, collection, start_year, end_year, landsat_name)
-                except Exception as e:
-                    logging.error(f'Failed to process pivot {row["ID"]}: {str(e)}')
-                    continue
-            else:
-                logging.info("Max tasks reached, waiting...")
-                time.sleep(600)  # Wait for 10 minutes before checking again
-                self.process_group(pivot_group, collection, start_year, end_year, landsat_name)
-                break
-
-    def export_pivot(self, row, collection, start_year, end_year, landsat_name):
+    def export_pivot(self, row, collection_path, start_year, end_year, landsat_name):
         geom = row['geometry']
-        month = random.randint(1, 13)
-        year = random.randint(start_year, end_year)
-        buffer_distance = 1000 / 100000
         ee_geom = ee.Geometry(geom.__geo_interface__)
-        filtered = collection.filterDate(f'{year}-{month:02d}-01', f'{year}-{month:02d}-28')\
-                             .filterBounds(ee_geom)\
-                             .filter(ee.Filter.lt('CLOUD_COVER', 10))
+        collection = ee.ImageCollection(collection_path)\
+                      .filterDate(f'{start_year}-01-01', f'{end_year}-12-31')\
+                      .filterBounds(ee_geom)\
+                      .filter(ee.Filter.lt('CLOUD_COVER', 10))
+        scaled = collection.map(self.apply_scale_factors)
+        masked = scaled.map(self.qa_mask)
         pivot_id = row['ID']
-        clipped_image = ee.Image(filtered.first()).toFloat()
-        description = f'CenterPivot_{pivot_id}_{year}_{month:02d}_{landsat_name}'
+        description = f'CenterPivot_{pivot_id}_{landsat_name}_TimeSeries'
         export_options = {
-            'image': clipped_image,
+            'image': masked.median().toFloat(),
             'description': self.clean_description(description),
             'folder': 'TOTAL_TIF_REQUEST',
             'scale': 30,
@@ -82,7 +62,15 @@ class LandsatDataExporter:
         }
         task = ee.batch.Export.image.toDrive(**export_options)
         task.start()
-        logging.info(f'Started export task for pivot {pivot_id} for {landsat_name} in {year}-{month:02d}')
+        logging.info(f'Started export task for pivot {pivot_id} for {landsat_name} time series')
+
+    def apply_scale_factors(self, image):
+        optical_bands = image.select('SR_B.*').multiply(0.0000275).add(-0.2)
+        thermal_band = image.select('ST_B10').multiply(0.00341802).add(149.0)
+        return image.addBands(optical_bands, None, True).addBands(thermal_band, None, True)
+
+    def qa_mask(self, image):
+        return image.updateMask(image.select('QA_PIXEL').bitwiseAnd(int('11111', 2)).eq(0))
 
     def check_active_tasks(self):
         tasks = ee.batch.Task.list()
